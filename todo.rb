62e7409c490b45cb663ae20735d22b8c167685ec
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-23 06:40:04 +0530

    Update todo.rb

--- todo.rb
+++ todo.rb
@@ -5,7 +5,7 @@ class Todo < ActiveRecord::Base
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
-    "#{display_status} #{todo_text} #{display_date}"
+    "#{id} #{display_status} #{todo_text} #{display_date}"
   end
   
    def self.to_displayable_list
