38051fa381243e6d3b5cec67f93f288da39a6292
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-24 06:49:43 +0530

    Update todo.rb

--- todo.rb
+++ todo.rb
@@ -2,35 +2,45 @@ require 'active_record'
 
 class Todo < ActiveRecord::Base
   
-  def to_displayable_string
-    display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
-    "#{id} #{display_status} #{todo_text} #{display_date}"
-  end
+ def initialize(text, due_date, completed)
+     @text=text
+     @due_date=due_date
+     @completed=completed
+ end
   
-   def self.to_displayable_list
+ def to_displayable_string
+    display_status = "[#{@completed ? "X" : " "}]"
+    display_date = @due_date unless (@due_date ==Date.today)
+    "#{id} #{display_status} #{@text} #{display_date}"
+ end
+    attr_accessor :text
+    attr_accessor :due_date
+    attr_accessor :completed
+  
+ def self.to_displayable_list
     all.map {|todo| todo.to_displayable_string }
-   end
+ end
   
-  def due_today?
+ def due_today?
     due_date == Date.today
-  end
+ end
   
-   def overdue?
+ def overdue?
     due_date < Date.today
-  end
+ end
 
-  def due_later?
+ def due_later?
     due_date > Date.today
-  end
+ end
 
  def self.show_list
+    puts "my todolist \n"
     puts "Overdue\n"
     message_display = Todo.where("due_date < ?",Date.today).map { |todo| todo.to_displayable_string }
     puts message_display
     
     puts "\nDue Today\n"
-    message_displayt = Todo.where("due_date = ?",Date.today).map { |todo| todo.to_displayable_string }
+    message_display = Todo.where("due_date = ?",Date.today).map { |todo| todo.to_displayable_string }
     puts message_display
 
     puts "\nDue Later\n"
@@ -39,7 +49,7 @@ class Todo < ActiveRecord::Base
   end
 
   def self.add_task(todo1)
-    Todo.create!(todo_text: todo1[:todo_text], due_date: Date.today + todo1[:due_in_days], completed: false)
+    Todo.create!(text: todo1[:text], due_date: Date.today + todo1[:due_in_days], completed: false)
   end
 
   def self.mark_as_complete(todo_id)
