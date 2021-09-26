1d3d75b434ba7e4d99cc080128ecaa71bb6bc6f9
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-26 19:34:22 +0530

    Update complete_todo.rb

--- complete_todo.rb
+++ complete_todo.rb
@@ -7,6 +7,6 @@ Todo.show_list
 puts "Which todo do you want to mark as complete? (Enter id): "
 todo_id = gets.strip.to_i
 
-todo = Todo.mark_as_complete(todo_id)
+todo = Todo.mark_as_complete!(todo_id)
 
 puts todo.to_displayable_string
